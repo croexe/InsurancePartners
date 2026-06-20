70a3822045289593d67a9d34aa155644fffba706
croexe <irinejs@gmail.com>
2026-06-20 11:47:29 +0200

    Service Results added

---
+++ Partners.Core/Results/PartnerServiceResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Partners.Core.Results
+{
+    public class PartnerServiceResult
+    {
+        public bool Success { get; set; }
+        public int? PartnerId { get; set; }
+        public List<string> Errors { get; set; } = [];
+
+        public static PartnerServiceResult Ok(int partnerId) =>
+            new() { Success = true, PartnerId = partnerId };
+
+        public static PartnerServiceResult Fail(params string[] errors) =>
+            new() { Success = false, Errors = errors.ToList() };
+    }
+}
---
+++ Partners.Core/Results/PolicyServiceResult.cs
@@ -0,0 +1,17 @@
+﻿using Partners.Core.DTOs.Responses;
+
+namespace Partners.Core.Results
+{
+    public class PolicyServiceResult
+    {
+        public bool Success { get; set; }
+        public PolicyResponse? Policy { get; set; }
+        public List<string> Errors { get; set; } = [];
+
+        public static PolicyServiceResult Ok(PolicyResponse policy) =>
+            new() { Success = true, Policy = policy };
+
+        public static PolicyServiceResult Fail(params string[] errors) =>
+            new() { Success = false, Errors = errors.ToList() };
+    }
+}
