fe2984c1336b1f77551f639b8d1c900d30a48491
croexe <irinejs@gmail.com>
2026-06-20 10:47:42 +0200

    Dal Helper Added

---
+++ Partners.Dal/Helpers/PartnerRow.cs
@@ -0,0 +1,40 @@
+﻿using Partners.Core.Enums;
+using Partners.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Partners.Dal.Helpers
+{
+    internal sealed class PartnerRow
+    {
+        public int Id { get; init; }
+        public string FirstName { get; init; } = string.Empty;
+        public string LastName { get; init; } = string.Empty;
+        public string? Address { get; init; }
+        public string PartnerNumber { get; init; } = string.Empty;
+        public string? CroatianPIN { get; init; }
+        public int PartnerTypeId { get; init; }
+        public DateTime CreatedAtUtc { get; init; }
+        public string CreateByUser { get; init; } = string.Empty;
+        public bool IsForeign { get; init; }
+        public string? ExternalCode { get; init; }
+        public string Gender { get; init; } = string.Empty;
+
+        public Partner ToPartner() => new()
+        {
+            Id = Id,
+            FirstName = FirstName,
+            LastName = LastName,
+            Address = Address,
+            PartnerNumber = PartnerNumber,
+            CroatianPIN = CroatianPIN,
+            PartnerTypeId = (PartnerType)PartnerTypeId,
+            CreatedAtUtc = CreatedAtUtc,
+            CreateByUser = CreateByUser,
+            IsForeign = IsForeign,
+            ExternalCode = ExternalCode,
+            Gender = Enum.Parse<Gender>(Gender, ignoreCase: true)
+        };
+    }
+}
