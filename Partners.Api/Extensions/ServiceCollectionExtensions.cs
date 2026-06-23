b443bc8a8e1e01fa3172dbe382a1a727615e17a7
croexe <irinejs@gmail.com>
2026-06-23 21:52:05 +0200

    Extensions Added

---
+++ Partners.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Partners.Api.ErrorHandling;
+using Partners.Api.Notifications;
+using Partners.Core.Contracts;
+using Partners.Core.Services;
+using Partners.Dal.Database;
+using Partners.Dal.Repositories;
+using System.Text.Json.Serialization;
+
+namespace Partners.Api.Extensions;
+
+internal static class ServiceCollectionExtensions
+{
+    public static IServiceCollection ApplyWienConfiguration(
+        this IServiceCollection services,
+        IWebHostEnvironment environment,
+        IConfiguration configuration)
+    {
+        services.AddOpenApi();
+
+        services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+
+        services.AddSignalR();
+
+        services.AddSingleton<IDbConnectionFactory>(_ =>
+            new SqlConnectionFactory(
+                configuration.GetConnectionString("InsurancePartnersDb")!));
+
+        services.AddScoped<IPartnerRepository, PartnerRepository>();
+        services.AddScoped<IPolicyRepository, PolicyRepository>();
+        services.AddScoped<IPartnerService, PartnerService>();
+        services.AddScoped<IPolicyService, PolicyService>();
+        services.AddScoped<IPartnerNotifier, SignalRPartnerNotifier>();
+
+        services.AddProblemDetails();
+        services.AddExceptionHandler<ProblemExceptionHandler>();
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowFrontend", policy =>
+            {
+                policy
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .SetIsOriginAllowed(_ => true) // development
+                    .AllowCredentials();
+            });
+        });
+
+        return services;
+    }
+}
---
+++ Partners.Api/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Partners.Api.Hubs;
+
+namespace Partners.Api.Extensions;
+
+internal static class WebApplicationExtensions
+{
+    public static WebApplication ApplyWienWebApplicationConfiguration(this WebApplication app)
+    {
+        app.UseExceptionHandler();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.UseHttpsRedirection();
+
+        app.UseCors("AllowFrontend");
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.MapHub<PartnerHub>("/hubs/partners");
+
+        return app;
+    }
+}
