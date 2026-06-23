ba1e8efc4a7f2d63b06b87fc64202a1fa2124036
croexe <irinejs@gmail.com>
2026-06-23 21:53:46 +0200

    Program.cs reorganised

--- Partners.Api/Program.cs
+++ Partners.Api/Program.cs
@@ -1,59 +1,8 @@
-using Microsoft.AspNetCore.Connections;
-using Partners.Api.Hubs;
-using Partners.Api.Notifications;
-using Partners.Core.Contracts;
-using Partners.Core.Services;
-using Partners.Dal.Database;
-using Partners.Dal.Repositories;
+using Partners.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
-});
-
-builder.Services.AddSignalR();
-builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
-    new SqlConnectionFactory(
-        builder.Configuration.GetConnectionString("InsurancePartnersDb")!));
-builder.Services.AddScoped<IPartnerRepository, PartnerRepository>();
-builder.Services.AddScoped<IPolicyRepository, PolicyRepository>();
-builder.Services.AddScoped<IPartnerService, PartnerService>();
-builder.Services.AddScoped<IPolicyService, PolicyService>();
-
-builder.Services.AddScoped<IPartnerNotifier, SignalRPartnerNotifier>();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend", policy =>
-    {
-        policy
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .SetIsOriginAllowed(_ => true)
-            .AllowCredentials();
-    });
-});
+builder.Services.ApplyWienConfiguration(builder.Environment, builder.Configuration);
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseCors("AllowFrontend");
-
-app.UseAuthorization();
-app.UseDefaultFiles();
-app.UseStaticFiles();
-app.MapControllers();
-
-app.MapHub<PartnerHub>("/hubs/partners");
-
+app.ApplyWienWebApplicationConfiguration();
 app.Run();
