c2a57bfbd554b2a464b2b517fb0fc7e2a18b4de8
croexe <irinejs@gmail.com>
2026-07-01 09:45:24 +0200

    Connection string constant Fix

--- backend/Partners.Api/Extensions/Configurations/PersistenceExtensions.cs
+++ backend/Partners.Api/Extensions/Configurations/PersistenceExtensions.cs
@@ -8,14 +8,16 @@ namespace Partners.Api.Extensions.Configurations;
 internal static class PersistenceExtensions
 {
     // Pristup podacima: EF Core DbContext (Identity) + Dapper connection factory + repozitoriji.
+    private const string ConnectionStringName = "InsurancePartnersDb";
+
     public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("InsurancePartnersDb")));
+            options.UseSqlServer(configuration.GetConnectionString(ConnectionStringName)));
 
         services.AddSingleton<IDbConnectionFactory>(_ =>
             new SqlConnectionFactory(
-                configuration.GetConnectionString("InsurancePartnersDb")!));
+                configuration.GetConnectionString(ConnectionStringName)!));
 
         services.AddScoped<IPartnerRepository, PartnerRepository>();
         services.AddScoped<IPolicyRepository, PolicyRepository>();
